f4b9e9276fa46d51940c3ba00e528cc05ac44ed5
w33d-w33d <chenyuqian0122@outlook.com>
2023-05-01 22:09:17 +0800

    Update Epoch.sol

--- contracts/Epoch.sol
+++ contracts/Epoch.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.0;
 
 library Epoch {
-    // 增加时代值
+    // 增加epoch值
     function incrementEpoch(uint _currentEpoch) public pure returns (uint) {
         return _currentEpoch + 1;
     }
 
-    // 检查时代是否达到设定上限
+    // 检查epoch是否达到设定上限
     function isEpochLimitReached(uint _currentEpoch, uint _epochLimit) public pure returns (bool) {
         return _currentEpoch >= _epochLimit;
     }
